637887733c3d9b8add03ddcbbaf32e3ad295580e
Pushpankar Singh <kpushpankar2@gmail.com>
2024-06-01 04:40:38 +0530

    add ordernow buttom functionality

--- public/js/app.js
+++ public/js/app.js
@@ -27883,8 +27883,8 @@ function _initStripe() {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\realtime-pizza-app-node-express-mongo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\realtime-pizza-app-node-express-mongo\resources\scss\app.scss */"./resources/scss/app.scss");
+__webpack_require__(/*! C:\Users\PUSHPANKAR\Desktop\realtime-pizza-app-node-express-mongo\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\PUSHPANKAR\Desktop\realtime-pizza-app-node-express-mongo\resources\scss\app.scss */"./resources/scss/app.scss");
 
 
 /***/ })
